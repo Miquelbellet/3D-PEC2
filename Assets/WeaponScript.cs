771d9e0378f48efb8b2c47b748355574e638a641
Miquel Bellet <miquel.bellet@gmail.com>
2020-10-29 01:10:43 +0100

    Initial commit

---
+++ Assets/WeaponScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public GameObject decalPrefab;
+    public AudioClip AK_fireClip;
+    public float AK_shootingCadency;
+    public Vector3 AK_recoil;
+
+    private AudioSource weaponAS;
+    private bool hasShot;
+    private float time;
+    void Start()
+    {
+        weaponAS = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (!hasShot && time <= AK_shootingCadency)
+        {
+            hasShot = true;
+            weaponAS.PlayOneShot(AK_fireClip);
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)), out hit))
+            {
+                GameObject bullet = Instantiate(decalPrefab,
+                        hit.point + hit.normal * 0.01f,
+                        Quaternion.FromToRotation(Vector3.forward, -hit.normal)
+                    );
+                Destroy(bullet, 8f);
+            }
+        }
+        else if (time >= AK_shootingCadency)
+        {
+            time = 0;
+            hasShot = false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# PEC2 - Template
+
+En este proyecto encontraréis los ejemplos de los materiales de este módulo, tanto de código como de funcionalidades de Unity. 
+
+Así como una base por si la queréis utilizar para empezar vuestros proyectos o trastear mientras aprendéis los materiales.
